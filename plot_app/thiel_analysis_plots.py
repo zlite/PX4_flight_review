434f13a1a3a896f560fc2e72e94924381ccef15a
zlite <chris@3dr.com>
2020-10-22 22:21:52 -0700

    Even more minimal

--- plot_app/thiel_analysis_plots.py
+++ plot_app/thiel_analysis_plots.py
@@ -40,37 +40,18 @@ This page shows the correspondance between a simulated and a real flight log.
 
   #  plots = thiel_analysis.startserver
 
-    rate_topic_name = 'vehicle_angular_velocity'
-    rate_field_names = ['xyz[0]', 'xyz[1]', 'xyz[2]']
-
 
 
     for index, axis in enumerate(['roll', 'pitch', 'yaw']):
         axis_name = axis.capitalize()
-        # rate
         data_plot = DataPlot(data, plot_config, 'actuator_controls_0',
                             y_axis_label='[deg/s]', title=axis_name+' Angular Rate',
                             plot_height='small',
                             x_range=x_range)
-
-        thrust_max = 200
-        actuator_controls = data_plot.dataset
-        data_plot.add_graph([lambda data: ("rate"+str(index),
-                                         np.rad2deg(data[rate_field_names[index]]))],
-                             colors3[0:1], [axis_name+' Rate Estimated'], mark_nan=True)
         data_plot.change_dataset('vehicle_rates_setpoint')
         data_plot.add_graph([lambda data: (axis, np.rad2deg(data[axis]))],
                             colors3[1:2], [axis_name+' Rate Setpoint'],
                             mark_nan=True, use_step_lines=True)
-        axis_letter = axis[0].upper()
-
-        rate_int_limit_param = 'MC_' + axis_letter + 'R_INT_LIM'
-        if rate_int_limit_param in ulog.initial_parameters:
-            rate_int_limit = '[-{0:.0f}, {0:.0f}]'.format(
-                ulog.initial_parameters[rate_int_limit_param]*100)
-        data_plot.change_dataset('rate_ctrl_status')
-        data_plot.add_graph([lambda data: (axis, data[axis+'speed_integ']*100)],
-                            colors3[2:3], [axis_name+' Rate Integral '+rate_int_limit])
         plot_flight_modes_background(data_plot, flight_mode_changes)
 
         if data_plot.finalize() is not None: plots.append(data_plot.bokeh_plot)
